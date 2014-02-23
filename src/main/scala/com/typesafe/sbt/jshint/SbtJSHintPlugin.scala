0bc349006a132e6f1be3be9c4494c38dcb01dfcb
Christopher Hunt <huntchr@gmail.com>
2014-02-23 17:22:59 +1100

    Incorporated the notion of source file tasks as distinct from other possibilities including test tasks and pipeline tasks. In addition the jsTask function has been simplified in terms of its parameter requirements.
    Also added in the new PathMappings behaviour.

--- src/main/scala/com/typesafe/sbt/jshint/SbtJSHintPlugin.scala
+++ src/main/scala/com/typesafe/sbt/jshint/SbtJSHintPlugin.scala
@@ -7,6 +7,7 @@ import com.typesafe.sbt.web.incremental._
 import sbt.File
 import scala.Some
 import com.typesafe.sbt.jse.SbtJsTaskPlugin
+import com.typesafe.sbt.web.PathMappings
 
 /**
  * The sbt plugin plumbing around the JSHint library.
@@ -15,9 +16,7 @@ object SbtJSHintPlugin extends SbtJsTaskPlugin {
 
   object JshintKeys {
 
-    val jshint = TaskKey[Int]("jshint", "Perform JavaScript linting.")
-
-    val jshintOptions = TaskKey[String]("jshint-options", "An array of jshint options to pass to the linter. This options are found via jshint-resolved-config. If there is no config then the options will be specified such that the JSHint defaults are used.")
+    val jshint = TaskKey[PathMappings]("jshint", "Perform JavaScript linting.")
 
     val config = SettingKey[Option[File]]("jshint-config", "The location of a JSHint configuration file.")
     val resolvedConfig = TaskKey[Option[File]]("jshint-resolved-config", "The actual location of a JSHint configuration file if present. If jshint-config is none then the task will seek a .jshintrc in the project folder. If that's not found then .jshintrc will be searched for in the user's home folder. This behaviour is consistent with other JSHint tooling.")
@@ -28,41 +27,42 @@ object SbtJSHintPlugin extends SbtJsTaskPlugin {
   import SbtJsTaskPlugin.JsTaskKeys._
   import JshintKeys._
 
-  def jshintSettings = inTask(jshint)(jsTaskSpecificUnscopedSettings) ++ Seq(
+  val jshintSettings = inTask(jshint)(
 
-    shellFile in jshint := "jshint-shell.js",
-    fileInputHasher in jshint := OpInputHasher[File](source => OpInputHash.hashString(source + "|" + jshintOptions.value)),
+    jsTaskSpecificUnscopedSettings ++ Seq(
+      shellFile := "jshint-shell.js",
+      fileFilter in Assets := (jsFilter in Assets).value,
+      fileFilter in TestAssets := (jsFilter in TestAssets).value,
 
-    config := None,
-    resolvedConfig := {
-      config.value.orElse {
-        val JsHintRc = ".jshintrc"
-        val projectRc = baseDirectory.value / JsHintRc
-        if (projectRc.exists()) {
-          Some(projectRc)
-        } else {
-          val homeRc = file(System.getProperty("user.home")) / JsHintRc
-          if (homeRc.exists()) {
-            Some(homeRc)
+      config := None,
+      resolvedConfig := {
+        config.value.orElse {
+          val JsHintRc = ".jshintrc"
+          val projectRc = baseDirectory.value / JsHintRc
+          if (projectRc.exists()) {
+            Some(projectRc)
           } else {
-            None
+            val homeRc = file(System.getProperty("user.home")) / JsHintRc
+            if (homeRc.exists()) {
+              Some(homeRc)
+            } else {
+              None
+            }
           }
-        }
-      }: Option[File]
-    },
-    jshintOptions := {
-      resolvedConfig.value
-        .map(IO.read(_))
-        .getOrElse("{}"): String
-    },
+        }: Option[File]
+      },
+      jsOptions := {
+        resolvedConfig.value
+          .map(IO.read(_))
+          .getOrElse("{}"): String
+      },
 
-    jshint in Assets := jsTask(jshint, Assets, jsFilter, jshintOptions, fileInputHasher, "JavaScript linting").value,
-    jshint in TestAssets := jsTask(jshint, TestAssets, jsFilter, jshintOptions, fileInputHasher, "JavaScript test linting").value,
-    jshint := (jshint in Assets).value,
+      fileInputHasher := OpInputHasher[File](source => OpInputHash.hashString(source + "|" + (jsOptions in jshint).value)),
 
-    jsTasks in Assets <+= (jshint in Assets),
-    jsTasks in TestAssets <+= (jshint in TestAssets)
+      taskMessage in Assets := "JavaScript linting",
+      taskMessage in TestAssets := "JavaScript test linting"
 
-  )
+    )
+  ) ++ addJsSourceFileTasks(jshint)
 
 }
