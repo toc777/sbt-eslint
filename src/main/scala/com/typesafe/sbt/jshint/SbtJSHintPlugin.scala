6796ebf35038b7e2150c87b9dc1cb6477854426f
Christopher Hunt <huntchr@gmail.com>
2014-03-01 12:18:29 +1100

    Pushed config in relation to the plugin only, to the global scope for easier setting.
    Also applied some changes as the result of source file plugins becoming resource generators.

--- src/main/scala/com/typesafe/sbt/jshint/SbtJSHintPlugin.scala
+++ src/main/scala/com/typesafe/sbt/jshint/SbtJSHintPlugin.scala
@@ -15,7 +15,7 @@ object SbtJSHintPlugin extends SbtJsTaskPlugin {
 
   object JshintKeys {
 
-    val jshint = TaskKey[Seq[PathMapping]]("jshint", "Perform JavaScript linting.")
+    val jshint = TaskKey[Seq[File]]("jshint", "Perform JavaScript linting.")
 
     val config = SettingKey[Option[File]]("jshint-config", "The location of a JSHint configuration file.")
     val resolvedConfig = TaskKey[Option[File]]("jshint-resolved-config", "The actual location of a JSHint configuration file if present. If jshint-config is none then the task will seek a .jshintrc in the project folder. If that's not found then .jshintrc will be searched for in the user's home folder. This behaviour is consistent with other JSHint tooling.")
@@ -26,31 +26,31 @@ object SbtJSHintPlugin extends SbtJsTaskPlugin {
   import SbtJsTaskPlugin.JsTaskKeys._
   import JshintKeys._
 
-  val jshintSettings = inTask(jshint)(
-
+  val jshintSettings = Seq(
+    config := None,
+    resolvedConfig := {
+      config.value.orElse {
+        val JsHintRc = ".jshintrc"
+        val projectRc = baseDirectory.value / JsHintRc
+        if (projectRc.exists()) {
+          Some(projectRc)
+        } else {
+          val homeRc = file(System.getProperty("user.home")) / JsHintRc
+          if (homeRc.exists()) {
+            Some(homeRc)
+          } else {
+            None
+          }
+        }
+      }: Option[File]
+    }
+  ) ++ inTask(jshint)(
     jsTaskSpecificUnscopedSettings ++ Seq(
       moduleName := "jshint",
       shellFile := "jshint-shell.js",
       fileFilter in Assets := (jsFilter in Assets).value,
       fileFilter in TestAssets := (jsFilter in TestAssets).value,
 
-      config := None,
-      resolvedConfig := {
-        config.value.orElse {
-          val JsHintRc = ".jshintrc"
-          val projectRc = baseDirectory.value / JsHintRc
-          if (projectRc.exists()) {
-            Some(projectRc)
-          } else {
-            val homeRc = file(System.getProperty("user.home")) / JsHintRc
-            if (homeRc.exists()) {
-              Some(homeRc)
-            } else {
-              None
-            }
-          }
-        }: Option[File]
-      },
       jsOptions := {
         resolvedConfig.value
           .map(IO.read(_))
